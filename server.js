10de516ba43ff0470d2a7bafd7110e3759ca0c26
Fraser Elliott <fraserelliott@hotmail.com>
2025-08-03 19:45:56 +0100

    updated logs for debugging deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 console.log("Working directory:", process.cwd());
 console.log("Env vars:", {
-  DATABASE_URL: process.env.TEST_VAR,
+  TEST_VAR: process.env.TEST_VAR,
 });
 
 const { sequelize, testConnection } = require("./config/connection");
